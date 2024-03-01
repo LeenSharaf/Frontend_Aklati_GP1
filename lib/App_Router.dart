7f221eea6208dad5abd57610f180190292ec8bca
LeenSharaf <94807919+LeenSharaf@users.noreply.github.com>
2024-03-01 21:12:01 +0200

    Add files via upload

---
+++ lib/App_Router.dart
@@ -0,0 +1,24 @@
+
+import 'package:flutter/material.dart';
+
+class AppRouter {
+  AppRouter._();
+  static AppRouter router = AppRouter._();
+  GlobalKey<NavigatorState> navigatorKey = GlobalKey();
+
+  push(Widget name) {
+    navigatorKey.currentState!.push(MaterialPageRoute(builder: (context) => name,));
+  }
+
+  pushReplace(Widget name) {
+    navigatorKey.currentState!.pushReplacement(MaterialPageRoute(builder: (context) => name,));
+  }
+
+  pop() {
+    navigatorKey.currentState!.pop();
+  }
+
+  // popUntil(String name) {
+  //   navigatorKey.currentState!.popUntil(ModalRoute.withName(name));
+  // }
+}
