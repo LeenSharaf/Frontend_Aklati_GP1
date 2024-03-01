2278df2a1c7ab0b7068fb3427d95cb8ad64a8d1a
LeenSharaf <94807919+LeenSharaf@users.noreply.github.com>
2024-03-01 20:32:58 +0200

    discount.dart

--- lib/Pages/discount.dart
+++ lib/Pages/discount.dart
@@ -4,10 +4,12 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:test_homee/Pages/admain_add.dart';
+import 'package:provider/provider.dart';
+import 'package:roaa/providers/AppProvider.dart';
+import '../Pages/admain_add.dart';
 //import 'package:test_homee/Shop/shop.dart';
 import 'package:http/http.dart' as http;
-import 'package:test_homee/constran.dart';
+import '../constran.dart';
 
 
 //import 'package:settings_ui/pages/settings.dart';
@@ -54,96 +56,106 @@ final List <String>  _typesName =[
   //bool? check2 = false, check3 = false;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      
-      appBar: AppBar(
-        title: Text("Add Discount",
-            style: TextStyle(
-                color: Colors.white)),
-        backgroundColor: kPrimaryColor,
-        elevation: 1,
-        leading: IconButton(
-          icon: const Icon(
-            Icons.arrow_back,
-            color: Colors.white,
-          ),
-          onPressed: () {
-Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) => creat(
-                                            
-                                          )));
-          },
-        ),
-      ),
-      body:Center(
-        
-        child: Container(
-          padding: EdgeInsets.only(left: 16, top: 50, right: 16),
-          child: GestureDetector(
-              onTap: () {
-                
-
-              },
-              child: ListView(children: [
-                SizedBox(height: 20),
-                      // Padding(padding: padding) , 
-                Text('Discount for your types:',
-                style: TextStyle(fontFamily:"Mulish-VariableFont" , fontSize: 20 , fontWeight: FontWeight.bold ),),  
-                 const SizedBox(
-                  height: 30,
-                ),
-
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                
-                labelText: 'Enter discount',
-                icon: Icon(Icons.percent),
-                border:OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10)
-                )
-                
+    return Consumer<AppProvider>(
+      builder: (context,provider,x) {
+        return Scaffold(
+          
+          appBar: AppBar(
+            title: Text("Add Discount",
+                style: TextStyle(
+                    color: Colors.white)),
+            backgroundColor: kPrimaryColor,
+            elevation: 1,
+            leading: IconButton(
+              icon: const Icon(
+                Icons.arrow_back,
+                color: Colors.white,
               ),
+              onPressed: () {
+Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => AdminHome(
+                                                
+                                              )));
+              },
             ),
-     
-              const SizedBox(
-                  height: 25,
-                ),
+          ),
+          body:Center(
+            
+            child: Container(
+              padding: EdgeInsets.only(left: 16, top: 50, right: 16),
+              child: GestureDetector(
+                  onTap: () {
+                    
 
-                        
-                               
-                                // DropdownButton<String>(
-                                //   alignment: AlignmentDirectional.centerEnd,
-                                //   value: item_cate,
-                                //   icon: const Icon(
-                                //     Icons.arrow_downward,
-                                //     size: 15,
-                                //   ),
-                                //   elevation: 10,
-                                //   style: const TextStyle(
-                                //       fontSize: 20,
-                                //       color: Colors.black,
-                                //       fontFamily: "Mulish-VariableFont",
-                                //       fontWeight: FontWeight.bold),
-                                //       borderRadius: BorderRadius.all(Radius.circular(10)),
-                                  
-                                //   onChanged: (String? value) {
-                                //     // This is called when the user selects an item.
-                                //     setState(() {
-                                //       item_cate = value!;
-                                //     });
-                                //   },
-                                //   items: list.map<DropdownMenuItem<String>>(
-                                //       (String value) {
-                                //     return DropdownMenuItem<String>(
-                                //       value: value,
-                                //       child: Text(value),
-                                //     );
-                                //   }).toList(),
-                                // ),
-                                Container(
+                  },
+                  child: ListView(children: [
+                    SizedBox(height: 20),
+                          // Padding(padding: padding) , 
+                    Text('Discount for your types:',
+                    style: TextStyle(fontFamily:"Mulish-VariableFont" , fontSize: 20 , fontWeight: FontWeight.bold ),),  
+                     const SizedBox(
+                      height: 30,
+                    ),
+
+                TextField(
+                  controller: provider.discountController,
+                  onChanged: (value) {
+                    if(value.isEmpty){
+                      value='0';
+                      provider.discountController.text=value;
+                    }
+
+                  },
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    
+                    labelText: 'Enter discount',
+                    icon: Icon(Icons.percent),
+                    border:OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10)
+                    )
+                    
+                  ),
+                ),
+         
+                  const SizedBox(
+                      height: 25,
+                    ),
+
+                            
+                                   
+                                    // DropdownButton<String>(
+                                    //   alignment: AlignmentDirectional.centerEnd,
+                                    //   value: item_cate,
+                                    //   icon: const Icon(
+                                    //     Icons.arrow_downward,
+                                    //     size: 15,
+                                    //   ),
+                                    //   elevation: 10,
+                                    //   style: const TextStyle(
+                                    //       fontSize: 20,
+                                    //       color: Colors.black,
+                                    //       fontFamily: "Mulish-VariableFont",
+                                    //       fontWeight: FontWeight.bold),
+                                    //       borderRadius: BorderRadius.all(Radius.circular(10)),
+                                      
+                                    //   onChanged: (String? value) {
+                                    //     // This is called when the user selects an item.
+                                    //     setState(() {
+                                    //       item_cate = value!;
+                                    //     });
+                                    //   },
+                                    //   items: list.map<DropdownMenuItem<String>>(
+                                    //       (String value) {
+                                    //     return DropdownMenuItem<String>(
+                                    //       value: value,
+                                    //       child: Text(value),
+                                    //     );
+                                    //   }).toList(),
+                                    // ),
+                                    Container(
 padding: const EdgeInsets.only(left: 30.0 , right: 30.0),
 // decoration: BoxDecoration(
 //   border: Border.all(color: Colors.black, width: 2.0),
@@ -152,51 +164,47 @@ padding: const EdgeInsets.only(left: 30.0 , right: 30.0),
 child: DropdownButton<String>(
   hint: Text('Select Type'),
 dropdownColor: Colors.grey,
-                                  elevation: 5,
+                                      elevation: 5,
 
-                                  icon: const Icon(
-                                    Icons.arrow_drop_down, 
-                                  ),
-                                  iconSize: 36.0,
-                                  isExpanded: true,
-                                  value:item_cate ,
+                                      icon: const Icon(
+                                        Icons.arrow_drop_down, 
+                                      ),
+                                      iconSize: 36.0,
+                                      isExpanded: true,
+                                      value:provider.consenItem ,
 
-                                  style: const TextStyle(
-                                      fontSize: 20,
-                                      color: Colors.black,
-                                      fontFamily: "Mulish-VariableFont",
-                                      fontWeight: FontWeight.bold),
-                                      borderRadius: BorderRadius.all(Radius.circular(10)),
-                                  
-                                  onChanged: (String? value) {
-                                    setState(() {
-                                      item_cate = value!;
-                                    });
-                                  },
-                                     items: _typesName.map<DropdownMenuItem<String>>(
-                                      (String value) {
-                                    return DropdownMenuItem<String>(
-                                      value: value,
-                                      child: Text(value),
-                                    );
-                                  }).toList(),
-                                ),
+                                      style: const TextStyle(
+                                          fontSize: 20,
+                                          color: Colors.black,
+                                          fontFamily: "Mulish-VariableFont",
+                                          fontWeight: FontWeight.bold),
+                                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                                      
+                                      onChanged: (value) {
+                                        provider.chooseItem(value);
+                                      },
+                                         items: provider.userItems.map<DropdownMenuItem<String>>(
+                                          ( value) {
+                                        return DropdownMenuItem<String>(
+                                          value: value.id,
+                                          child: Text(value.name),
+                                        );
+                                      }).toList(),
+                                    ),
 
 
-                                ),
-                        
-                           
-             
+                                    ),
+                            
+                               
+                 
 
 
 
 
   
-       
+           
 
-                  SizedBox(height: 40,),
-
-
+                      SizedBox(height: 40,),
 
 
 
@@ -204,75 +212,79 @@ dropdownColor: Colors.grey,
 
 
 
-              
-                Column(
+
+
                   
-                  //crossAxisAlignment: CrossAxisAlignment.end,
+                    Column(
+                      
+                      //crossAxisAlignment: CrossAxisAlignment.end,
 
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    
-                    children: [
-                     
-                      ElevatedButton(
-                        onPressed: () {
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        
+                        children: [
                          
-                            //  add(item_name.text, item_price.text, item_desc.text, item_discount.text , item_cate, _selectedGender, base64Image  );
-                        },
-                        style: OutlinedButton.styleFrom(
-                         padding: const EdgeInsets.symmetric(horizontal: 50),
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20)),
-                          backgroundColor: Color.fromARGB(255, 146, 14, 14),
-                        ),
+                          ElevatedButton(
+                            onPressed: () async{
+                             await provider.addDisCount();
+                                //  add(item_name.text, item_price.text, item_desc.text, item_discount.text , item_cate, _selectedGender, base64Image  );
+                            },
+                            style: OutlinedButton.styleFrom(
+                             padding: const EdgeInsets.symmetric(horizontal: 50),
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20)),
+                              backgroundColor: Color.fromARGB(255, 146, 14, 14),
+                            ),
 
-                        //  elevation: 2,
+                            //  elevation: 2,
 
-                        child: Text("Update",
-                            style: TextStyle(
-                                fontSize: 20,
-                                letterSpacing: 1,
-                                color: Colors.white)),
-                      ),
-                     // Spacer(),
-
-                      OutlinedButton(
-                          // padding: const EdgeInsets.symmetric(horizontal: 50),
-                          //shape: RoundedRectangleBorder(
-                          //    borderRadius: BorderRadius.circular(20)),
-
-                          onPressed: () {                      Navigator.pop(context);
-},
-                          style: OutlinedButton.styleFrom(
-                           padding: const EdgeInsets.symmetric(horizontal: 50),
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(27)),
-                            backgroundColor: Color.fromARGB(255, 221, 121, 156),
+                            child: Text("Update",
+                                style: TextStyle(
+                                    fontSize: 20,
+                                    letterSpacing: 1,
+                                    color: Colors.white)),
                           ),
-                          //  elevation: 2,
+                         // Spacer(),
 
-                          child: Text("Cancel",
-                              style: TextStyle(
-                                  fontSize: 18,
-                                  letterSpacing: 1,
-                                  color: Colors.white))),
+                          OutlinedButton(
+                              // padding: const EdgeInsets.symmetric(horizontal: 50),
+                              //shape: RoundedRectangleBorder(
+                              //    borderRadius: BorderRadius.circular(20)),
+
+                              onPressed: () {                      Navigator.pop(context);
+},
+                              style: OutlinedButton.styleFrom(
+                               padding: const EdgeInsets.symmetric(horizontal: 50),
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(27)),
+                                backgroundColor: Color.fromARGB(255, 221, 121, 156),
+                              ),
+                              //  elevation: 2,
+
+                              child: Text("Cancel",
+                                  style: TextStyle(
+                                      fontSize: 18,
+                                      letterSpacing: 1,
+                                      color: Colors.white))),
 
 
 
 
 
 
-                    ]),
+                        ]),
 
-                // buildTextField("Product Details"),
-              ]))),
-
-
+                    // buildTextField("Product Details"),
+                  ]))),
 
 
 
-              
-    
-    ));
+
+
+                  
+        
+        ));
+      }
+    );
   }
 }
 
