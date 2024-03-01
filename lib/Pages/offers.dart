5c8481aff2ea6d8b4aa872bd14278d12a915f04e
LeenSharaf <94807919+LeenSharaf@users.noreply.github.com>
2024-03-01 20:35:45 +0200

    offers.dart

--- lib/Pages/offers.dart
+++ lib/Pages/offers.dart
@@ -94,12 +94,15 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:test_homee/Shop/shopData.dart';
-import 'package:test_homee/Shop/testfordetail.dart';
+import 'package:provider/provider.dart';
+import 'package:roaa/Admin_cart/details.dart';
+import 'package:roaa/providers/AppProvider.dart';
+import '../Shop/shopData.dart';
+import '../Shop/testfordetail.dart';
 
 
 import 'package:http/http.dart' as http;
-import 'package:test_homee/constran.dart';
+import '../constran.dart';
 
 class Offers extends StatefulWidget {
 
@@ -114,102 +117,110 @@ class _FavouriteState extends State<Offers> {
   // final int id;
   // _FavouriteState({required this.id});
 
-  void initState() {
-    super.initState();
-    getData();
-    // _restorepersistedPrefrrence();
-  }
+  // void initState() {
+  //   super.initState();
+  //   getData();
+  //   // _restorepersistedPrefrrence();
+  // }
 
-  var shopdata = [];
-  getData() async {
-    var url =
-        'http://172.19.215.243/connect/getfrom_Fav.php';
+  // var shopdata = [];
+  // getData() async {
+  //   var url =
+  //       'http://172.19.215.243/connect/getfrom_Fav.php';
 
-    var response = await http.post(Uri.parse(url), body: {
-     // "id": widget.id.toString(),
+  //   var response = await http.post(Uri.parse(url), body: {
+  //    // "id": widget.id.toString(),
       
-    });
+  //   });
 
-    setState(() {
-      shopdata = json.decode(response.body);
-    });
+  //   setState(() {
+  //     shopdata = json.decode(response.body);
+  //   });
 
-    print(shopdata);
-    return shopdata;
-  }
+  //   print(shopdata);
+  //   return shopdata;
+  // }
 
-  Future DeleteFromFavourite(String item_img) async {
-    var url =
-        'http://192.168.1.109/connect/deletefrom_Fav.php';
+  // Future DeleteFromFavourite(String item_img) async {
+  //   var url =
+  //       'http://192.168.1.109/connect/deletefrom_Fav.php';
 
-    var response = await http.post(Uri.parse(url), body: {
-      "item_img": item_img,
-    });
+  //   var response = await http.post(Uri.parse(url), body: {
+  //     "item_img": item_img,
+  //   });
 
-    var data = json.decode(response.body);
-    print(data);
-  }
+  //   var data = json.decode(response.body);
+  //   print(data);
+  // }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "Offers",
-          style:
-              TextStyle(color: Colors.white, fontFamily: "Mulish-VariableFont"),
-        ),
-        backgroundColor: kPrimaryColor,
-        centerTitle: true,
-      ),
-      body: ListView.separated(
-        separatorBuilder: (BuildContext context, int index) {
-          return SizedBox(
-            height: 5,
-          );
-        },
-        scrollDirection: Axis.vertical,
-        itemCount: shopdata.length,
-        itemBuilder: (context, index) {
-          return Card(
-              child: ListTile(
-            leading:
-                Image.asset(shopdata[index]['item_img'], height: 80, width: 80),
-            title: Row(
-              children: [
-                Text(
-                  shopdata[index]['item_name'],
-                  style: TextStyle(
-                      fontSize: 15,
-                      color: Colors.black,
-                      fontFamily: "Mulish-VariableFont",
-                      fontWeight: FontWeight.bold),
-                ),
-                // IconButton(
-                //     icon: Icon(Icons.favorite, color: Colors.red, size: 15),
-                //     onPressed: () => {
-                //           DeleteFromFavourite(shopdata[index]['item_img']),
-                //         })
-              ],
+    return Consumer<AppProvider>(
+      builder: (context,provider,x) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text(
+              "Offers",
+              style:
+                  TextStyle(color: Colors.white, fontFamily: "Mulish-VariableFont"),
             ),
-            subtitle: Text(
-              shopdata[index]['item_desc'],
-              style: TextStyle(
-                  fontSize: 12,
-                  color: Colors.black,
-                  fontFamily: "Mulish-VariableFont",
-                  fontWeight: FontWeight.bold),
-            ),
-
-            trailing: Text(shopdata[index]['item_price'] + "\$",
-                style: TextStyle(
-                    fontSize: 13,
-                    color: Colors.black,
-                    fontFamily: "Mulish-VariableFont",
-                    fontWeight: FontWeight.bold)),
-          ));
-        },
-      ),
+            backgroundColor: kPrimaryColor,
+            centerTitle: true,
+          ),
+          body: ListView.separated(
+            
+            separatorBuilder: (BuildContext context, int index) {
+              return SizedBox(
+                height: 5,
+              );
+            },
+            scrollDirection: Axis.vertical,
+            itemCount: provider.items.where((element) => element.discount>0).toList().length,
+            itemBuilder: (context, index) {
+              return InkWell(
+                onTap: ()=>Navigator.of(context).push(MaterialPageRoute(builder: ((context) => Itm(shopdata: provider.items.where((element) => element.discount>0).toList()[index])))),
+                child: Card(
+                    child: ListTile(
+                  leading:
+                      Image.network(provider.items.where((element) => element.discount>0).toList()[index].img, height: 80, width: 80),
+                  title: Row(
+                    children: [
+                      Text(
+                        provider.items.where((element) => element.discount>0).toList()[index].name,
+                        style: TextStyle(
+                            fontSize: 15,
+                            color: Colors.black,
+                            fontFamily: "Mulish-VariableFont",
+                            fontWeight: FontWeight.bold),
+                      ),
+                      // IconButton(
+                      //     icon: Icon(Icons.favorite, color: Colors.red, size: 15),
+                      //     onPressed: () => {
+                      //           DeleteFromFavourite(shopdata[index]['item_img']),
+                      //         })
+                    ],
+                  ),
+                  subtitle: Text(
+                    provider.items.where((element) => element.discount>0).toList()[index].discount.toString(),
+                    style: TextStyle(
+                        fontSize: 15,
+                        color: Colors.black,
+                        fontFamily: "Mulish-VariableFont",
+                        fontWeight: FontWeight.bold),
+                  ),
+              
+                  trailing: Text(provider.items.where((element) => element.discount>0).toList()[index].price.toString() + "\$",
+                      style: TextStyle(
+                          fontSize: 15,
+                          color: Colors.black,
+                          fontFamily: "Mulish-VariableFont",
+                          fontWeight: FontWeight.bold)),
+                )),
+              );
+            },
+          ),
+        );
+      }
     );
   }
 }
