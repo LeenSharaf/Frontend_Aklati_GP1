381aeeff3dba659ca525e5771c21ff396f2bb517
LeenSharaf <94807919+LeenSharaf@users.noreply.github.com>
2024-03-01 21:01:30 +0200

    body.dart

--- lib/profile/body.dart
+++ lib/profile/body.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:test_homee/profile/menufor_profile.dart';
+import 'package:provider/provider.dart';
+import 'package:roaa/providers/AppProvider.dart';
+import '../profile/menufor_profile.dart';
 import 'user_info.dart';
 import 'package:flutter_svg/svg.dart';
 
@@ -10,37 +12,41 @@ class Body_profile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        children: <Widget>[
-          const Info(
-           image: "lib/images/pic.png",
-            name: "Jawabreh Roaa",
-            email: "RoaaJawabreh@gmail.com",
+    return Consumer<AppProvider>(
+      builder: (context,provider,c) {
+        return SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+               Info(
+               image: "lib/images/pic.png",
+                name: provider.loggedUser.fullName,
+                email: provider.loggedUser.mobile,
+              ),
+              SizedBox(height: 20), //20
+              ProfileMenuItem(
+                iconSrc: "lib/icons/add_to_cart.svg",
+                title: "shopping cart",
+                press: () {}, 
+              ),
+              ProfileMenuItem(
+                iconSrc: "lib/icons/heart_fill.svg",
+                title: "Favorite Page",
+                press: () {},
+              ),
+              ProfileMenuItem(
+                iconSrc: "lib/icons/bookmark_fill.svg",
+                title: "About Us",
+                press: () {},
+              ),
+              ProfileMenuItem(
+                iconSrc: "lib/icons/info.svg",
+                title: "Help",
+                press: () {},
+              ),
+            ],
           ),
-          SizedBox(height: 20), //20
-          ProfileMenuItem(
-            iconSrc: "lib/icons/add_to_cart.svg",
-            title: "shopping cart",
-            press: () {}, 
-          ),
-          ProfileMenuItem(
-            iconSrc: "lib/icons/heart_fill.svg",
-            title: "Favorite Page",
-            press: () {},
-          ),
-          ProfileMenuItem(
-            iconSrc: "lib/icons/bookmark_fill.svg",
-            title: "About Us",
-            press: () {},
-          ),
-          ProfileMenuItem(
-            iconSrc: "lib/icons/info.svg",
-            title: "Help",
-            press: () {},
-          ),
-        ],
-      ),
+        );
+      }
     );
   }
 }
