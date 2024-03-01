f91a6e01b2801339edf5f2f34bb33e252d9a890b
LeenSharaf <94807919+LeenSharaf@users.noreply.github.com>
2024-03-01 20:42:16 +0200

    shop.dart

--- lib/Shop/shop.dart
+++ lib/Shop/shop.dart
@@ -1,7 +1,9 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:test_homee/products_screen.dart';
+import 'package:provider/provider.dart';
+import 'package:roaa/providers/AppProvider.dart';
+import '../products_screen.dart';
 import 'package:http/http.dart' as http;
 
 
@@ -22,34 +24,14 @@ class Shop extends StatefulWidget {
 class _ShopState extends State<Shop> {
   void initState() {
     super.initState();
-    getData();
   }
 
   static var shopdata = [];
-  List display_List = List.from(shopdata);
+  
   //var ip = Connection.IP;
-  getData() async {
-    var url = 'http://192.168.1.109/connect/getdata.php';
 
-    var response = await http.get(Uri.parse(url));
 
-    setState(() {
-      shopdata = json.decode(response.body);
-      display_List = List.from(shopdata);
-    });
 
-    print(display_List);
-    return shopdata;
-  }
-
-  void updateList(String value) {
-    setState(() {
-      display_List = shopdata
-          .where((element) =>
-              element['name'].toLowerCase().contains(value.toLowerCase()))
-          .toList();
-    });
-  }
 
   // final List<Shopdata> shopdata = [
   //   Shopdata(
@@ -107,21 +89,23 @@ class _ShopState extends State<Shop> {
           '${itemdesc[index]}', '${itemprice[index]}'));*/
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: ListView(children: [
-      
-            Container(
-              alignment: Alignment.centerLeft,
-              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-              child: Text(
-                " Types for you",
-                style: TextStyle(
-                         fontFamily: 'Varela',
-                                          fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black),
-              ),
-            ),
+    return Consumer<AppProvider>(
+      builder: (context,provider,c) {
+        return Scaffold(
+            body: ListView(children: [
+          
+                Container(
+                  alignment: Alignment.centerLeft,
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                  child: Text(
+                    " Types for you",
+                    style: TextStyle(
+                             fontFamily: 'Varela',
+                                              fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black),
+                  ),
+                ),
 //             GridView(
 //               physics: NeverScrollableScrollPhysics(),
 //               shrinkWrap: true,
@@ -153,7 +137,7 @@ class _ShopState extends State<Shop> {
 //                       Row(
 //                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
 //                         children: [
-                         
+                             
 //                           IconButton(
 //                             onPressed: () => {
 //                              Navigator.of(context).push(MaterialPageRoute(
@@ -194,11 +178,11 @@ class _ShopState extends State<Shop> {
 //                             margin: EdgeInsets.all(5),
 
 
-                            
+                                
 //                             child: Image.asset(
 //                               sh.Image,
 //                               //"lib/images/ric.jpg",
-                              
+                                  
 //                               height: 100,
 //                               width: 100,
 //                             )),
@@ -248,140 +232,129 @@ class _ShopState extends State<Shop> {
 //               .toList(),
 //             ),
 GridView.builder(
-              itemCount: display_List.length,
-              physics: NeverScrollableScrollPhysics(),
-              shrinkWrap: true,
-              gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-                  maxCrossAxisExtent: 200,
-                  childAspectRatio: .69,
-                  crossAxisSpacing: 10,
-                  mainAxisSpacing: 5),
-              itemBuilder: (context, index) => 
-                 // padding: EdgeInsets.only(left: 15, right: 15, top: 7),
-                   Container(
-                    padding: EdgeInsets.only(left: 15, right: 15, top: 0),
-                  margin: EdgeInsets.symmetric(vertical: 8, horizontal: 10),
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(20),
-               boxShadow: [
-      BoxShadow(
-        color: Colors.grey.withOpacity(0.5),
-        spreadRadius: 5,
-        blurRadius: 7,
-        offset: Offset(0, 3), // changes position of shadow
-      ),
-    ],
-           
-                        ),
-                    child: Column(
-                      children: [
-                         Row(
+                  itemCount: provider.filteredItems.length,
+                  physics: NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                      maxCrossAxisExtent: 200,
+                      childAspectRatio: .69,
+                      crossAxisSpacing: 10,
+                      mainAxisSpacing: 5),
+                  itemBuilder: (context, index) => 
+                     // padding: EdgeInsets.only(left: 15, right: 15, top: 7),
+                       InkWell(
+                        onTap: (){
+                          Navigator.of(context).push(MaterialPageRoute(builder: (context)=>Itm(shopdata: provider.filteredItems[index])));
+                        },
+                         child: Container(
+                          padding: EdgeInsets.only(left: 15, right: 15, top: 0),
+                                             margin: EdgeInsets.symmetric(vertical: 8, horizontal: 10),
+                                             decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(20),
+                                          boxShadow: [
+                                 BoxShadow(
+                                   color: Colors.grey.withOpacity(0.5),
+                                   spreadRadius: 5,
+                                   blurRadius: 7,
+                                   offset: Offset(0, 3), // changes position of shadow
+                                 ),
+                               ],
+                                      
+                              ),
+                              child: Column(
+                    children: [
+                      Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                         
-                          IconButton(
-                            onPressed: () => {
-                            
-                            },
-                            icon: Icon(
-                              Icons.favorite_border,
-                              color: Colors.black,
-                              //size: 15,
-                            ),
-                          ),
-                        ],
-                      ),
-                        InkWell(
-                          onTap: () {
-                            Navigator.of(context).push(MaterialPageRoute(
-                                builder: (context) => Itm(
-                                      shopdata: data(
-                                          name: display_List[index]['item_name'],
-                                          Image: display_List[index]['item_img'],
-                                          desc: display_List[index]['item_desc'],
-                                          price: display_List[index]['item_price'], 
-                                          namepage: display_List[index]['item_page'], 
-                                         // logo: display_List[index]['page_logo']
-                                          ),
-
-                                      //id: widget.id,
-                                    )));
-                          },
-                          child: Container(
-                              margin: EdgeInsets.all(2),
-                              child: Image.asset(
-                                display_List[index]['item_img'],
-                                height: 100,
-                               width: 100,
-                              )
-                              ),
-                        ),
-                        Container(
-                          padding: EdgeInsets.only(bottom: 3),
-                          alignment: Alignment.centerLeft,
-                          child: Text(
-                            display_List[index]['item_name'],
-                            style: TextStyle(
-                                fontSize: 15,
-                                color: Colors.black,
-                                fontFamily: "Mulish-VariableFont",
-                                fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                        Container(
-                          alignment: Alignment.centerLeft,
-                          child: Text(
-                            display_List[index]['item_desc'],
-                            style: TextStyle(
-                                fontSize: 10,
-                                color: Colors.black,
-                                fontFamily: "Mulish-VariableFont",
-                                fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                        // Padding(
-                        //   padding: EdgeInsets.symmetric(vertical: 5),
-                        //   child: Row(
-                        //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        //     children: [
-                        //       Text(display_List[index]['item_price'] + "\$",
-                        //           style: TextStyle(
-                        //               fontSize: 10,
-                        //               color: Colors.black,
-                        //               fontFamily: "Mulish-VariableFont",
-                        //               fontWeight: FontWeight.bold)),
-                        //                                           Icon(Icons.shopping_cart)
-
-                        //     ],
-                        //   ),
-                        // )
-                        Padding(
-                        padding: EdgeInsets.symmetric(vertical: 4),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(display_List[index]['item_price'] + "\$",
+                          Container(
+                             padding: EdgeInsets.all(2),
+                                child: Text(
+                                  provider.filteredItems[index].discount.toString() + "%",
                                   style: TextStyle(
-                                      fontSize: 13,
+                                      fontSize: 15,
                                       color: Colors.black,
                                       fontFamily: "Mulish-VariableFont",
-                                      fontWeight: FontWeight.bold)),
-                            // Text(sh.price,
-                            //     style: TextStyle(
-                            //         fontSize: 13,
-                            //         color: Colors.black,
-                            //        // fontFamily: "Mulish-VariableFont",
-                            //         fontWeight: FontWeight.bold)),
-                            Icon(Icons.shopping_cart)
-                          ],
-                        ),
-                      )
-                      ],
-                    ),
-                  )
-            ),
-          ]));
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                          
+                          
+                         
+                         IconButton(
+                                  onPressed: () async {
+                                    if(provider.filteredItems[index].isLiked){
+                                      await provider.disLikeItem(provider.filteredItems[index].id!);
+                                    }
+                                    else{
+                                    await provider.likeItem(provider.filteredItems[index].id!);
+                                    }
+                                  },
+                                  icon:provider.filteredItems[index].isLiked?Icon(Icons.favorite,color: Colors.red,): Icon(
+                                    Icons.favorite_border,
+                                    color: Colors.black,
+                                    //size: 15,
+                                  ),
+                                ),
+                          
+                          
+                        ],
+                      ),
+                      InkWell(
+                                onTap: () {
+                                  Navigator.of(context).push(MaterialPageRoute(
+                                      builder: (context) => Itm(
+                                            shopdata:provider.filteredItems[index],
+                       
+                                            //id: widget.id,
+                                          )));
+                                },
+                                child: Container(
+                                    margin: EdgeInsets.all(2),
+                                    child: Image.network(
+                                      provider.filteredItems[index].img,
+                                      height: 100,
+                                     width: 100,
+                                    )
+                                    ),
+                              ),
+                     Container(
+                                padding: EdgeInsets.only(bottom: 3),
+                                alignment: Alignment.centerLeft,
+                                child: Text(
+                                  provider.filteredItems[index].name,
+                                  style: TextStyle(
+                                      fontSize: 15,
+                                      color: Colors.black,
+                                      fontFamily: "Mulish-VariableFont",
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                      
+                     Padding(
+                              padding: EdgeInsets.symmetric(vertical: 4),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(provider.filteredItems[index].price.toString() + "\₪",
+                                        style: TextStyle(
+                                            fontSize: 15,
+                                            color: Colors.black,
+                                            fontFamily: "Mulish-VariableFont",
+                                            fontWeight: FontWeight.bold)),
+                                  
+                                ],
+                              ),
+                            )
+                    ],
+                  ),
+                          
+                                             ),
+                       )
+                ),
+              ]));
+      }
+    );
     
   }
 }
