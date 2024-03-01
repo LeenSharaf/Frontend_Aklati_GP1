051f3f88f3afcc28e3c147d8d1d188b3e365a497
LeenSharaf <94807919+LeenSharaf@users.noreply.github.com>
2024-03-01 20:29:15 +0200

    inReview.dart

--- lib/Admin_cart/inReview.dart
+++ lib/Admin_cart/inReview.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:roaa/Admin_cart/OrderAWidget.dart';
+import 'package:roaa/providers/AppProvider.dart';
 
 
 import '../constran.dart';
@@ -7,35 +10,43 @@ import 'cart_inRevw.dart';
 class inRev extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-    appBar: buildAppBar(context),
+    return Consumer<AppProvider>(
+      builder: (context,provider,x) {
+        return Scaffold(
+        appBar: buildAppBar(context),
 
-      body: ListView(children: [
-      Container(height: 700,padding: EdgeInsets.only(top: 15),
-      decoration: BoxDecoration(color:Color(0xFFEDECF2),
-      borderRadius:
-       BorderRadius.only(topLeft:Radius.circular(35) ,topRight: Radius.circular(35)
-       )
-       ),
-       child: Column(children: [
-        cartItem(),
-       Container(//decoration: BoxDecoration
-      // (borderRadius: BorderRadius.circular(10))
-       margin: EdgeInsets.symmetric(vertical: 20,horizontal: 15),
-       padding: EdgeInsets.all(10),
-       child: Row(children: [
-        Container(decoration: BoxDecoration(color: Color.fromARGB(255, 146, 14, 14),
-        borderRadius: BorderRadius.circular(20)),)
-       ],),)],),
-       
-      
-       
-       )
-      ],
-      ),
-     // bottomNavigationBar: cartBottom(),
-     
-      );
+          body: ListView(children: [
+          Container(height: 700,padding: EdgeInsets.only(top: 15),
+          decoration: BoxDecoration(color:Color(0xFFEDECF2),
+          borderRadius:
+           BorderRadius.only(topLeft:Radius.circular(35) ,topRight: Radius.circular(35)
+           )
+           ),
+           child: Column(children: [
+            Expanded(
+              child: ListView.builder(itemBuilder: (context,index){
+                return OrderAWidget(order: provider.userOrdersForReview[index]);
+              },itemCount: provider.userOrdersForReview.length,),
+            ),
+           Container(//decoration: BoxDecoration
+          // (borderRadius: BorderRadius.circular(10))
+           margin: EdgeInsets.symmetric(vertical: 20,horizontal: 15),
+           padding: EdgeInsets.all(10),
+           child: Row(children: [
+            Container(decoration: BoxDecoration(color: Color.fromARGB(255, 146, 14, 14),
+            borderRadius: BorderRadius.circular(20)),)
+           ],),)],),
+           
+          
+           
+           )
+          ],
+          ),
+         // bottomNavigationBar: cartBottom(),
+         
+          );
+      }
+    );
   }
 }
 AppBar buildAppBar(BuildContext context) {
